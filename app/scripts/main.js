935c07d0eb8d2fac8b48a9338c271ff1791c04b0
kelvin@labtime.ufg.br <kelvin@labtime.ufg.br>
2018-07-31 10:29:02 -0300

    Structure base

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+/**
+ *
+ * main.js
+ * Script main of website Consulte Ambientes
+ * @author  Kélvin Santiago, http://github.com/kelvinsleonardo/
+ * @author  Rodrigo Lima, http://github.com/rodrilhp/
+ * @created 2018-07-31
+ *
+ */
+
+// Function executed when script is ready
+// Shorthand for $( document ).ready()
+$(function() {
+  console.log( "Script is ready!" );
+});
